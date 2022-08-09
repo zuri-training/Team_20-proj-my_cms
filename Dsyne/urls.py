219d04d5477903f3ec8cdee008ac4535e9d79bd9
MikeSoft007 <mekpenyong2@gmail.com>
2022-08-09 18:49:31 +0100

    updated

--- Dsyne/urls.py
+++ Dsyne/urls.py
@@ -14,28 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path, include
-
+from django.urls import path, include
+from account import views
+# importing the re path
+from django.urls import re_path, include
 # for media handling
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-
-from account import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('register/', views.registration_view, name='register'),
-    path("admin-django/", admin.site.urls),
+    
     # Portfolio url
     path("portfolio/", include("portfolio.urls")),
     # blogPost url
     path("blog/", include("blog.urls")),
+    # url for blog restApi
+    path("api/v1/blog/", include("blog.api.urls")),
+    path('signin/', views.signin, name='signin'),
+    path('signin/login', views.signin, name='signin'),
     # path for django_cms
     re_path(r"^admin/", admin.site.urls),
     re_path(r"^", include("cms.urls")),
-    # url for blog restApi
-    path("api/v1/blog/", include("blog.api.urls")),
     # urls for media
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Templates/Core/Dsyne-contact-us-page/contact.js
@@ -0,0 +1,53 @@
+// Light mode/dark mode toggler
+const colorSwitch = document.getElementById('input-color-switch');
+
+colorSwitch.addEventListener('click', checkMode);
+function checkMode() {
+    console.log('checking')
+    if (colorSwitch.checked) {
+        darkModeOn()
+    } else {
+        darkModeOff()
+    }
+}
+
+function darkModeOn() {
+    document.body.classList.add("dark-mode")
+}
+function darkModeOff() {
+    document.body.classList.remove("dark-mode")
+}
+//logo
+
+//Navbar
+
+function classToggle() {
+    const navs = document.querySelectorAll(".nav-bar")
+    navs.forEach(nav =>
+        nav.classList.toggle('show')
+    );
+}
+document.querySelector('#menu').addEventListener('click', classToggle)
+
+//dropdown
+function dropMenu() {
+    const drops = document.querySelector('#drop-menu')
+    if (drops.style.display == "none") {
+        drops.style.display = "block"
+    } else {
+        drops.style.display = "none"
+    }
+}
+document.querySelector('#select-field').addEventListener('click', dropMenu)
+
+function formValidate() {
+    let x =
+        document.forms['contactUsForm'];
+    ['fullName'].values;
+    if (x == " ") {
+        alert('full name is required');
+        console.log("working")
+        return false;
+
+    }
+}
