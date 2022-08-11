18cbc7c232e5fd2adc3c294da8d19fbaa510d96b
macanotmarker <macasorekingdavid@gmail.com>
2022-08-11 21:32:24 +0100

    adjusted

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.portfolio, name="portfolio"),
     path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("index", views.index, name="index")
 ]
