0e73bd2063e969b6fcd61febbc9ac28b43a9fa65
ikechukwu mmachukwu <ikechukwummachukwu2020@gmail.com>
2022-08-11 20:38:35 +0100

    more routes

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,6 @@
 # from . import views
 # urlpatterns = [
 
-#  path('', views.home, name = "home"),
+# #  path('', views.home, name = "home"),
 #  path("signup/", views.SignUp.as_view(), name="signup"),
 # ]
