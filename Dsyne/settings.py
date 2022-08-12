da077525dfbb342ef08ede86c85f2709c9d88898
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-08-12 22:02:57 +0100

    change the database

--- Dsyne/settings.py
+++ Dsyne/settings.py
@@ -147,7 +147,7 @@ DATABASES = {}
 if DEBUG:
     DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "Dsyne.db",
     }
 else:
     DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
