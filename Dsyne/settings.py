73ecd0cc98052d392831ff0516a65a6b5a9506c6
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-08-12 20:43:23 +0100

    setup database

--- Dsyne/settings.py
+++ Dsyne/settings.py
@@ -134,18 +134,23 @@ WSGI_APPLICATION = "Dsyne.wsgi.application"
 
 # DATABASES = {}
 
-# if DEBUG:
-DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-# else:
+# # if DEBUG:
 # DATABASES = {
-#     "default": dj_database_url.config()
-# }
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         }
+#     }
 
+DATABASES = {}
+
+if DEBUG:
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+else:
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
