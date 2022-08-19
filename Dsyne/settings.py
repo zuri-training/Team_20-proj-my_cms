83b76af653a8637ae53189362e20568fa8df37ec
Ibukun Alesinloye <89009222+highb33kay@users.noreply.github.com>
2022-08-19 09:51:26 +0100

    Update settings.py

--- Dsyne/settings.py
+++ Dsyne/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost")
 
