83f88c2afb4716e5f28ea36fa37e9214cfb908b9
highb33kay <hiibeekayvibe@gmail.com>
2022-08-19 10:54:16 +0100

    added loging

--- Dsyne/settings.py
+++ Dsyne/settings.py
@@ -312,3 +312,26 @@ JAZZMIN_UI_TWEAKS = {
 #     'toolbar': 'CMS',
 #     'skin': 'moono',
 # }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': 'D:\home\site\wwwroot\myapp.log'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        }
+    }
+}
